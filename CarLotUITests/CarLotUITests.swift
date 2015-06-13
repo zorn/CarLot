26713009227fdd310bf5f83b5643f7fbb88d283f
Mike Zornek <mike@clickablebliss.com>
2015-06-13 09:54:38 -0700

    Adding new core data project.

---
+++ CarLotUITests/CarLotUITests.swift
@@ -0,0 +1,35 @@
+//
+//  CarLotUITests.swift
+//  CarLotUITests
+//
+//  Created by Michael D Zornek on 6/13/15.
+//  Copyright © 2015 Clickable Bliss. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class CarLotUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CarLot
 A demo Mac OS X app from the BNR Cocoa book.
+
+# Requirements
+
+Xcode 7 beta 1, Swift 2
